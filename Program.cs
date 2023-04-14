8f1ca2fccba5a495ad542c88526bd4dda4a742f4
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2023-04-14 20:07:59 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1   //fileSystemLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            if (args.Length > 0)
+            {
+                path = args[0];
+            }
+            else
+            {
+                path = Environment.CurrentDirectory;
+             
+            }
+            DirectoryInfo myDirInfo = new DirectoryInfo(path);
+            foreach (var item in myDirInfo.EnumerateDirectories())
+            {
+                Console.WriteLine($"Каталог {item}");
+            }
+            {
+                foreach (var item in myDirInfo.EnumerateFiles())
+                {
+                    Console.WriteLine($"Файл {item} имеет размер {item.Length} байт был изменен {item.LastWriteTime}");
+                }
+            }
+            Console.ReadKey();
+            }
+        }
+    }
+
